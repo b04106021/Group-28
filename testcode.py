21be08a9c155c12ab51241113b18390f54e783bc
scott020 <scott020@users.noreply.github.com>
2019-06-07 19:34:04 +0800

    Revert "對話更新"
    This reverts commit 1bbf7ae9c36e1af593d56045c6fb110c3f4c38a4.

--- testcode.py
+++ testcode.py
@@ -1,30 +1,26 @@
 from verify_studentid import funtion(is_student_number)
 
 from flask import Flask, request, abort
+from urllib.request import urlopen
+#from oauth2client.service_account import ServiceAccountCredentials
 
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError,LineBotApiError
 )
+
+################################
+
 from linebot.models import *
 
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+line_bot_api = LineBotApi("bz73tyjUExIIdgZ3h4VTsKC4L3Y/mmdoxW16daM8nMnjC5ii3wC23rRE0fF8Ry7+sFyINeMRHbWsdC9PY6F8xixBtnVYsPaTjF5zBfEzbywS7Lqkx1aK9JxJlbl5PnvHBAL1RiW3BD7wDoTd+QB0mwdB04t89/1O/w1cDnyilFU=")
 # Channel Secret
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-
-#第一則推播
-try:
-    line_bot_api.push_message(to, TextSendMessage(text='歡迎使用台大腳踏車機器人'))
-except LineBotApiError as e:
-    # error handle
-    raise e
-
-##選擇我要檢舉or我要查詢
+handler = WebhookHandler("eee486e5d9aa1ed8ff0ff78eef9fd1ef")
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -42,37 +38,28 @@ def callback():
     return 'OK'
 
 # 處理訊息
+# 做 chatbot 的人是要寫在這裡，下面的 code 只是測試用的。因為下面這樣寫，所以現在輸 Hi 會得到回應 Hello ，以此類推
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text = event.message.text
-    if #獲得我要檢舉的指令:
-        reply_text = '請告訴我又是哪個白癡亂停腳踏車呢，快告訴我他的學號?'
-        stat = 'report'
-    elif text.is_student_number() == True and stat == 'report':
-        reply_text = '我知道了，那台車在哪呢，我幫你呼叫水源阿北'
-    elif text == '':
-        reply_text = '收到了，但檢舉不附圖，此風不可長，快上傳證據照片'
-    elif event.message.type == 'image':
-        reply_text = '謝謝您的幫忙，打擊亂停自行車，人人有責~ 水源感謝您'
-#查詢的code
-    elif #獲得我要查詢的指令:
-        reply_text = '請輸入你的學號'
-        stat = 'search'
-    elif text.is_student_number == True and stat = 'search':
-        #尋找資料庫
-        if #在資料庫中:
+    print(event)
+    text=event.message.text
+
+    if (text=="Hi"):
+        reply_text = "Hello"
+        #Your user ID
+
+    elif(text=="你好"):
+        reply_text = "哈囉"
+    elif(text=="機器人"):
+        reply_text = "叫我嗎"
     else:
-        #回到原本選單
+        reply_text = text
+#如果非以上的選項，就會學你說話
+
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
-
-
-
-
 
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-def handle_message(event):
-    message = Text''
