a4378d3f42704c37e29f22820c67360923125a0e
b04106021 <51196373+b04106021@users.noreply.github.com>
2019-06-09 14:07:17 +0800

    Change db image type
    把違規照片改成 blob 格式

--- database_example.py
+++ database_example.py
@@ -1,9 +1,13 @@
 import database as db
 from datetime import datetime
 
+
+
 print('Recreate the database with dummies', db.recreate_database(dummy=True))
 print('Is b04106021 a bicycle:', db.valid_bicycle('b04106021'))
 print('Is b04106021-01 a bicycle:', db.valid_bicycle('b04106021-01'))
-print('Add a new violation:', db.add_violation('b04106021-01', '亂停車',
-    datetime(2019, 6, 8, 12, 0, 0), '管院門口', 'photo.jpg'))
+
+with open('bicycle_example.jpg', 'rb') as image:
+    print('Add a new violation:', db.add_violation('b04106021-01', '亂停車',
+        datetime(2019, 6, 8, 12, 0, 0), '管院門口', image.read()))
 print('Get b04106021-01 violation:', db.get_violation_by_bicycle('b04106021-01'))
